e02715387cfdda3029ab5359fc9b3ed7ed11a588
mnkhod <eikyuu.hoshi@gmail.com>
2020-04-21 02:21:12 +0800

    Adding Push Notif , Socket.IO

--- js/base.js
+++ js/base.js
@@ -1,5 +1,7 @@
 var submit = document.querySelector("#chat-submit");
 var chatbox = document.querySelector(".chat-box");
+var socket = io();
+
 
 submit.addEventListener('click', () => {
   let username = document.querySelector("#username-box");
@@ -13,12 +15,19 @@ submit.addEventListener('click', () => {
 
     $.post('http://localhost:4000/messages', data);
 
-    alert('Sended Data! Please Refresh Browser');
 
   }
 });
 
 function getData(){
+
+  socket.on("message", (item) => {
+	let new_msg_e = document.createElement("div");
+	new_msg_e.classList.add("msg-container");
+	new_msg_e.innerHTML = "<h1 class='name'>"+item.name+"</h1> <p class='msg'> "+item.msg+"</p>";
+	chatbox.appendChild(new_msg_e); 
+  });
+
   $.get('http://localhost:4000/messages', (data) => {
 
     for(item of data){
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var parser = require('body-parser');
 var app = express();
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.use(express.static(__dirname));
 app.use(parser.json());
 app.use(parser.urlencoded({extended: false}));
@@ -18,7 +21,12 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
   msgs.push(req.body);
+  io.emit('message' , req.body);
   res.sendStatus(200);
 });
 
-var server = app.listen(4000, () => console.log('server is listening on port ' , server.address().port));
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+var server = http.listen(4000, () => console.log('server is listening on port ' , server.address().port));
