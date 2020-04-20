7b6e4711f762d6e52bdf4eeef8357bafb007e9db
mnkhod <eikyuu.hoshi@gmail.com>
2020-04-21 02:01:39 +0800

    Added Data Endpoint, Post Request

--- js/base.js
+++ js/base.js
@@ -8,10 +8,13 @@ submit.addEventListener('click', () => {
   if(username.value == '' || msg.value == ''){
     alert("No Username Or Message");
   }else{
-    let new_msg_e = document.createElement("div");
-    new_msg_e.classList.add("msg-container");
-    new_msg_e.innerHTML = "<h1 class='name'>"+username.value+"</h1> <p class='msg'> "+msg.value+"</p>";
-    chatbox.appendChild(new_msg_e); 
+
+    let data = { name: username.value, msg: msg.value  };
+
+    $.post('http://localhost:4000/messages', data);
+
+    alert('Sended Data! Please Refresh Browser');
+
   }
 });
 
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
+var parser = require('body-parser');
 var app = express();
+
+app.use(express.static(__dirname));
+app.use(parser.json());
+app.use(parser.urlencoded({extended: false}));
+
 
 var msgs = [
   { name: 'Gabby' , msg: 'Go To Bed!' },
@@ -10,5 +16,9 @@ app.get('/messages', (req, res) => {
   res.send(msgs);
 });
 
-app.use(express.static(__dirname));
+app.post('/messages', (req, res) => {
+  msgs.push(req.body);
+  res.sendStatus(200);
+});
+
 var server = app.listen(4000, () => console.log('server is listening on port ' , server.address().port));
